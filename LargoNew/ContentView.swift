5c6fba42843a2791f1e6615b70cb32d8290f8c9e
Lars Donnevert <lars.donnevert@sourcesign.de>
2024-09-11 20:07:14 +0200

    initial commit

---
+++ LargoNew/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Largo
+//
+//  Created by Lars on 11.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LargoNew/LargoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LargoApp.swift
+//  Largo
+//
+//  Created by Lars on 11.09.24.
+//
+
+import SwiftUI
+
+@main
+struct LargoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
